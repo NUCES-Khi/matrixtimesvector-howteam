e97c8fefc63c2639afdec73052089e2bc05cb67d
k213088 <127028606+k213088@users.noreply.github.com>
2024-03-13 20:39:40 +0500

    Update batch.sh

--- assign1/batch.sh
+++ assign1/batch.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
 
+input_sizes=(64 128 256 512 1024 2048 4096 8192 16384 32768)
+
+for size in "${input_sizes[@]}"
+do
+    echo "Running programs for input size: $size"
+   
+    # Run program 1
+    echo "Running program 1"
+    time ./svmm.c $size $size
+   
+    # Run program 2
+    echo "Running program 2"
+    time ./naive.c $size $size
+   
+    # Run program 3 
+    echo "Running program 3"
+    time ./naivempi.c $size $size
+   
+    # Run program 4 
+    echo "Running program 4"
+    time ./tilingomp.c $size $size
+   
+    # Run program 5
+    echo "Running program 5"
+    time ./tilingmpi.c $size $size
+   
+    echo "---------------------------------------------------"
+done
